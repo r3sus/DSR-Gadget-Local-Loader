bbe68bbc7f9e578bd34f98052e7dc9d83b431507
Meikk99 <25267317+Meikk99@users.noreply.github.com>
2021-08-15 03:50:06 +0200

    Change update checker to forked repository

--- DSR-Gadget/FormMain.cs
+++ DSR-Gadget/FormMain.cs
@@ -51,7 +51,7 @@ namespace DSR_Gadget
             GitHubClient gitHubClient = new GitHubClient(new ProductHeaderValue("DSR-Gadget"));
             try
             {
-                Release release = await gitHubClient.Repository.Release.GetLatest("JKAnderson", "DSR-Gadget");
+                Release release = await gitHubClient.Repository.Release.GetLatest("Meikk99", "DSR-Gadget");
                 if (SemVersion.Parse(release.TagName) > System.Windows.Forms.Application.ProductVersion)
                 {
                     lblUpdate.Text = "New version available!";
