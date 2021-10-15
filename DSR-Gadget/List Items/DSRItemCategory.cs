360149cdbaf3116c09fcec50a9679cbec0d76a78
Norgaren <83100346+Nordgaren@users.noreply.github.com>
2021-10-15 02:46:56 -0700

    refactored category code to be legible

--- DSR-Gadget/List Items/DSRItemCategory.cs
+++ DSR-Gadget/List Items/DSRItemCategory.cs
@@ -35,7 +35,11 @@ namespace DSR_Gadget
                 {
                     var att = GetTxtResourceClass.RegexSplit(line, ",");
                     Array.ForEach<string>(att, x => att[Array.IndexOf<string>(att, x)] = x.Trim());
-                    All.Add(new DSRItemCategory(att[0].Trim(), Convert.ToInt32(att[1].Trim(), 16), GetTxtResourceClass.GetTxtResource(att[2].Trim()), bool.Parse(att[3])));
+                    var name = att[0].Trim();
+                    var categoryID = Convert.ToInt32(att[1].Trim(), 16);
+                    var itemList = GetTxtResourceClass.GetTxtResource(att[2].Trim());
+                    var showIDs = bool.Parse(att[3]);
+                    All.Add(new DSRItemCategory(name, categoryID, itemList, showIDs));
                 }
             };
         }
